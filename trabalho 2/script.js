dece046e90dcae7630c0d2bf0b06a5edf22b55c9
jamyllecesario <167343776+jamyllecesario@users.noreply.github.com>
2024-06-12 20:46:30 -0300

    Add files via upload

---
+++ trabalho 2/script.js
@@ -0,0 +1,43 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    cartItem.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+     cartItem.classList.toggle('active');
+     navbar.classList.remove('active');
+     searchForm.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+    
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+
+
